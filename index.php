00427c3450386ce4fb2f33157960a8d54cbf2bab
PixelArg <Agustinazar4c@gmail.com>
2015-11-16 03:12:59 -0300

    'cambios'

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ function simple_decrypt($text,$salt){
     return trim(mcrypt_decrypt(MCRYPT_RIJNDAEL_256, $salt, base64_decode($text), MCRYPT_MODE_ECB, mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB), MCRYPT_RAND)));
 }
 
-
+$enc_key = 'kjrghekjrhgluhfgliuehdbivudfhlsghfglj';
 
 $app = new \Slim\Slim();
 $app->config('databases', [
