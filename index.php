d703fe75e731ef0cb79be03b2fbb71c8ee8a8ba6
AriadnaGuitian <ariadnaguitian@gmail.com>
2015-12-04 21:34:45 -0300

    'fsda'

--- index.php
+++ index.php
@@ -265,7 +265,7 @@ $app->get('/imagenes/:Id', function ($Id) use ($app) {
 
 $app->get('/noticias', function () use ($app) {
 	$db = $app->db->getConnection();
-	$images = $db->table('noticias')->select('id', 'Titulo','Descripcion','created_at')->order by('Titulo')->get();
+	$images = $db->table('noticias')->select('id', 'Titulo','Descripcion','created_at')->orderby('Descripcion')->get();
 	$app->render(200,array('data' => $images));
 });
 
