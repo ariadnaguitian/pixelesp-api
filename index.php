9a295ccafd9fca49118874cb4bbd2d7bb51dd2b4
AriadnaGuitian <ariadnaguitian@gmail.com>
2016-06-17 22:20:45 -0300

    'fsfsfs'

--- index.php
+++ index.php
@@ -81,7 +81,7 @@ $app->post('/login', function () use ($app) {
 
 
 	$db = $app->db->getConnection();
-	$user =$db->table('usuarios')->select()->where('password', $password)->first();
+	$user =$db->table('usuarios')->select()->where('username', $username)->first();
     if(empty($user)){
         $app->render(500,array(
             'error' => TRUE,
