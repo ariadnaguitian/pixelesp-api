4633408210cbf69380ebf88455fa7ddc3d2ef45f
AriadnaGuitian <ariadnaguitian@gmail.com>
2016-06-18 01:17:04 -0300

    'fsfsfs'

--- index.php
+++ index.php
@@ -374,7 +374,7 @@ $app->get('/noticias', function () use ($app) {
 	->orderby('created_at','desc')
 	->get();
 	foreach ($images as $key => $value) {
-		$newscomment = newscomments::where('id_noticia', '=', $value->id)
+		$newscomment =  NewsComments::where('id_noticia', '=', $value->id)
 		->select('newscomments.*','usuarios.name')
 		->leftjoin('usuarios', 'usuarios.id', '=', 'newscomments.idusuario')
 		
@@ -665,7 +665,7 @@ $app->post('/newscomments', function () use ($app) {
 
 
 		
-    $newscomment = new newscomments();
+    $newscomment = new NewsComments();
     $newscomment->idusuario  = $idusuario;
     $newscomment->id_noticia = $id_noticia;
     $newscomment->text 		 = $text;
@@ -686,7 +686,7 @@ $app->put('/newscomments/:id', function ($id) use ($app) {
         ));
 	}
 
-	$newscomment = newscomments::find($id);
+	$newscomment = NewsComments::find($id);
 	if(empty($newscomment)){
 		$app->render(404,array(
 			'error' => TRUE,
@@ -698,7 +698,7 @@ $app->put('/newscomments/:id', function ($id) use ($app) {
     $app->render(200,array('data' => $newscomment->toArray()));
 });
 $app->get('/newscomments/:id', function ($id) use ($app) {
-	$newscomment = newscomments::find($id);
+	$newscomment = NewsComments::find($id);
 	if(empty($newscomment)){
 		$app->render(404,array(
 			'error' => TRUE,
@@ -708,7 +708,7 @@ $app->get('/newscomments/:id', function ($id) use ($app) {
 	$app->render(200,array('data' => $newscomment->toArray()));
 });
 $app->delete('/newscomments/:id', function ($id) use ($app) {
-	$newscomment = newscomments::find($id);
+	$newscomment = NewsComments::find($id);
 	if(empty($newscomment)){
 		$app->render(404,array(
 			'error' => TRUE,
