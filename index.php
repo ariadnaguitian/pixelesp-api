c0f131b348e614ee6c1b371c87ac7b4202a41a2c
AriadnaGuitian <ariadnaguitian@gmail.com>
2016-08-12 02:04:06 -0300

    'fasf'

--- index.php
+++ index.php
@@ -1640,7 +1640,34 @@ $app->get('/misfavoritosimglist', function () use ($app) {
 	$app->render(200,array('data' => $favoritosimg));
 });
 
-
+$app->post('/crearmensaje/:id_destino', function ($id_destino) use ($app) {
+		
+		$input = $app->request->getBody();
+		
+		$id_origen = $input['id_origen'];
+		$asunto = $input['asunto'];		
+		$mensaje = $input['mensaje'];
+		
+		if(empty($asunto)){
+			$app->render(500,array(
+				'error' => TRUE,
+				'msg'   => 'Por favor, ingrese un asunto del mensaje.',
+			));}
+		if(empty($mensaje)){
+			$app->render(500,array(
+				'error' => TRUE,
+				'msg'   => 'Por favor, ingrese un mensaje.',
+			));}
+		
+		$msg = new Mensaje();		
+		$msg->from 				= $id_origen;
+		$msg->to 				= $id_destino;
+		$msg->asunto			= $asunto;
+		$msg->msj 				= $mensaje;
+		$msg->save();
+		
+		$app->render(200);
+	});
 
 $app->run();
 ?>
