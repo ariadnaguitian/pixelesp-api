e33737e95a9d35ce55db32af41203a2787734d1f
AriadnaGuitian <ariadnaguitian@gmail.com>
2016-08-14 02:32:19 -0300

    'fasfas'

--- index.php
+++ index.php
@@ -598,8 +598,8 @@ $app->get('/imagenes/:id', function ($id) use ($app) {
 
 
 $imagen = Image::find($id);
-	$imagenusuario =  User::where('imagenes.id', '=', $imagen->$id)
-	->leftjoin('imagenes', 'imagenes.IdUsuario', '=', 'usuarios.id')
+	$imagenusuario =  Image::where('imagenes.id', '=', $imagen->$id)
+	->leftjoin('usuarios', 'imagenes.IdUsuario', '=', 'usuarios.id')
 	->select()->get();
  	if(empty($imagenusuario->toArray())){
  		$result = array();
